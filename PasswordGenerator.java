87a644ea3d31268385b9fb09224d95861ddc8f31
Aayush Saini <44221400+aayush1saini@users.noreply.github.com>
2020-03-24 00:56:06 +0530

    Update PasswordGenerator.java

--- PasswordGenerator.java
+++ PasswordGenerator.java
@@ -1,8 +1,13 @@
 package hobey;
 import java.util.Random;
+import java.io.FileWriter;
 public class PasswordGenerator {
-	final String alpha="bycxdwevjqirhsgtfukplomnaz";
-	final String caps="ZANMOLPKUFTGSHRIQJVEWDXCYB";
+	final String 
+
+alpha="bycxdwevjqirhsgtfukplomnaz";
+	final String 
+
+caps="ZANMOLPKUFTGSHRIQJVEWDXCYB";
 	final String nums="9257314068";
 	final String spc="&@%#(}[{]*}/!?^";
 	
@@ -10,48 +15,80 @@ public class PasswordGenerator {
 	{
 		String temp="";
 		
-		Random rand=new Random();
+		Random rand=new 
+
+Random();
 		int i=0;
 		while(i<3)
 		{
-			int alphaL=rand.nextInt(alpha.length());
-		    int capsL=rand.nextInt(caps.length());
+			int 
+
+alphaL=rand.nextInt(alpha.length());
+		    int capsL=rand.nextInt
+
+(caps.length());
 		    
-		    temp+=alpha.charAt(alphaL);
-		    temp+=caps.charAt(capsL);
+		    temp+=alpha.charAt
+
+(alphaL);
+		    temp+=caps.charAt
+
+(capsL);
 		    i++;
 		}
 		i=0;
 		while(i<2) 
 		{
-			int numsL=rand.nextInt(nums.length());
-			int spcL =rand.nextInt(spc.length());
+			int 
+
+numsL=rand.nextInt(nums.length());
+			int spcL 
+
+=rand.nextInt(spc.length());
 			
-			temp+=spc.charAt(spcL);
-			temp+=nums.charAt(numsL);
+			temp
+
++=spc.charAt(spcL);
+			temp
+
++=nums.charAt(numsL);
 			i++;
 		}
 		
 		
 		return temp;
 	}
-	public void shuffle(String temp)
+	public String shuffle(String temp)
 	{
 		
-		char temppswd[]=temp.toCharArray();
+		char temppswd
+
+[]=temp.toCharArray();
 		int x=1;
 		String password="";
-		Random rand=new Random();
-		while(x<=10) 
+		Random rand=new 
+
+Random();
+		while(x<=100) 
 		{
-			int i=rand.nextInt(temp.length());
-			int j=rand.nextInt(temp.length());
+			int 
+
+i=rand.nextInt(temp.length());
+			int 
+
+j=rand.nextInt(temp.length());
 		
 			if(i!=j)
 			{
-			   char t=temppswd[i];
-			   temppswd[i]=temppswd[j];
-			   temppswd[j]=t;
+			   char 
+
+t=temppswd[i];
+			   temppswd
+
+[i]=temppswd[j];
+			   temppswd
+
+[j]=t;
 			}
 			x++;
 			
@@ -59,15 +96,61 @@ public class PasswordGenerator {
 		
 		for(int i=0;i<10;i++)
 		{
-			password+=temppswd[i];
+			password
+
++=temppswd[i];
 		}
-		System.out.println(password);
+		return password;
 		
+	}
+	
+	public void writetofile(String 
+
+password)
+	{
+		try {
+			FileWriter 
+
+fw=new FileWriter("C:\\Users\\AAYUSH\
+
+\Documents\\java_projects\
+
+\PasswordGenerator\
+
+\PasswordGenerator_out.txt");
+			fw.write
+
+(password);
+			fw.flush();
+			fw.close();
+		}
+		catch(Exception e)
+		{
+			
+
+System.out.println("Something went wrong.....");
+			
+
+System.out.println(e);
+		}
+		finally 
+		{
+			
+
+System.out.println("Password successfully 
+
+Generated.....");
+		}
 	}
 
 	public static void main(String args[]) {
-		PasswordGenerator a=new PasswordGenerator();
+		PasswordGenerator a=new 
+
+PasswordGenerator();
 		String temp=a.generate();
-		a.shuffle(temp);
+		String password=a.shuffle
+
+(temp);
+		a.writetofile(password);
 	}
 }
